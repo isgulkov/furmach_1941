a29f56a577b280abe7673242549edb7205c18526
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-03-28 01:45:00 +0300

    Use sys.exit() in both __main__.py files insead of nested if-else

--- problem_1/__main__.py
+++ problem_1/__main__.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv, exit
 
 from solution import pr01
 from variants import get_dist_by_variant_number
@@ -6,7 +6,8 @@ from variants import get_dist_by_variant_number
 if __name__ == '__main__':
     if len(argv) != 2:
         print "Usage: python problem_1 [x], x - varik number"
-    else:
-        Dist = get_dist_by_variant_number(int(argv[1]))
+        exit()
+    
+    Dist = get_dist_by_variant_number(int(argv[1]))
 
-        pr01(Dist)
+    pr01(Dist)
--- problem_2/__main__.py
+++ problem_2/__main__.py
@@ -1,4 +1,4 @@
-from sys import argv
+from sys import argv, exit
 
 from solution import pr02a, pr02b
 from variants import get_dist_by_variant_number
@@ -6,8 +6,9 @@ from variants import get_dist_by_variant_number
 if __name__ == '__main__':
     if len(argv) != 2:
         print "Usage: python problem_2 [x], x - varik number"
-    else:
-        Dist = get_dist_by_variant_number(int(argv[1]))
+        exit()
 
-	    pr02a(Dist)
-	    pr02b(Dist)
+    Dist = get_dist_by_variant_number(int(argv[1]))
+
+    pr02a(Dist)
+    pr02b(Dist)
