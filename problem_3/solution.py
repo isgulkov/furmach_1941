b800f795ae91e597a6f6b02d797f551bc0801f8c
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-03-28 08:09:44 +0300

    Draw histogram and display rs on the plot

--- problem_3/solution.py
+++ problem_3/solution.py
@@ -1,4 +1,32 @@
+from scipy.stats import pearsonr, spearmanr
+
+from matplotlib import pyplot as plt, gridspec
+
+from os import system
 
 def pr03((xs, ys, )):
-	print xs[:3]
-	print ys[:3]
+
+    fig = plt.figure(figsize=(6, 7, ))
+    gs = gridspec.GridSpec(2, 1, height_ratios=[6, 1])
+
+    fig.add_subplot(gs[0])
+
+    plt.scatter(xs, ys, s=1.0)
+
+    fig.add_subplot(gs[1])
+
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_visible(False)
+    cur_axes.axes.get_yaxis().set_visible(False)
+
+    cur_axes.axes.set_xlim(0, 1)
+    cur_axes.axes.set_ylim(-3, 0)
+
+    plt.text(0.05, -1, "$r_{pearson} = %.4f$ with $p = %e$" % pearsonr(xs, ys), clip_on=False)
+    plt.text(0.05, -2, "$r_{spearman} = %.4f$ with $p = %e$" % spearmanr(xs, ys), clip_on=False)
+
+    r_spearman, p_spearman = spearmanr(xs, ys)
+
+    plt.savefig("~figure.png", dpi=300)
+
+    system("open ./~figure.png")
