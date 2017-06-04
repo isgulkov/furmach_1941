cd798960024b49d257e2f34cb5f86df7788c8f9f
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-06-05 01:09:33 +0300

    Render template with a dict of variables

--- problem_4/solution.py
+++ problem_4/solution.py
@@ -1,10 +1,17 @@
 import numpy as np
-
 from matplotlib import pyplot as plt, gridspec
 
 from os import system, path
 
 from jinja2 import Template
+
+def _render_template(template_vars):
+    template_path = path.join(path.dirname(__file__), 'output_template.md')
+
+    with open(template_path, mode='r') as f:
+        template = Template(f.read().decode('utf-8'))
+
+    return template.render(**template_vars)
 
 def pr04_part1((x2s, x3s, x4s, ys, ), var_number):
     y_vector = np.array(ys)
@@ -16,17 +23,21 @@ def pr04_part1((x2s, x3s, x4s, ys, ), var_number):
         x4s,
         ]).transpose()
 
+    template_vars = {}
+
+    template_vars['var_number'] = var_number
+
     # Find least-squares solution for X * beta = y
     beta_vector, residues, rank, s = np.linalg.lstsq(x_matrix, y_vector)
 
-    template_path = path.join(path.dirname(__file__), 'output_template.md')
+    template_vars['beta_hats'] = beta_vector.tolist()
 
     with open(template_path, mode='r') as f:
         template = Template(f.read().decode('utf-8'))
 
-    for line in template.render(var_number=var_number, beta_hats=beta_vector.tolist()).split('\n'):
-        print line.encode('utf-8')
+    result = _render_template(template_vars)
 
+    print result.encode('utf-8')
 
 
     # plt.scatter(x3s, ys, s=5.0)
