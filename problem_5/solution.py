f548f309bd0da9f22de24bf2dbe489f1cca392f7
Ilya Gulkov <isgulkov@edu.hse.ru>
2017-06-06 15:05:43 +0300

    Calculate RSS and R^2_adj for each model

--- problem_5/solution.py
+++ problem_5/solution.py
@@ -14,7 +14,7 @@ def _render_template(template_vars):
 
     return template.render(**template_vars)
 
-def _save_and_show_plot(price, x_matrix, beta_vector, filename="~figure.png"):
+def _save_and_show_plot(price, x_matrix, beta_vector, filename="~figure.png", yvar_name="price"):
     fig = plt.figure(figsize=(10, 5, ))
     gs = gridspec.GridSpec(1, 2, width_ratios=[1, 1])
 
@@ -24,19 +24,32 @@ def _save_and_show_plot(price, x_matrix, beta_vector, filename="~figure.png"):
 
     plt.scatter(price_hats, [price[i] - price_hats[i] for i in xrange(len(price))], s=5.0)
     plt.title("Residues with respect to fitted price")
-    plt.xlabel("$\\widehat{price}$")
-    plt.ylabel("$price - \\widehat{price}$")
+    plt.xlabel("$\\widehat{%s}$" % (yvar_name, ))
+    plt.ylabel("$%s - \\widehat{%s}$" % (yvar_name, yvar_name, ))
 
     fig.add_subplot(gs[1])
 
     plt.hist(price_hats, int(len(price_hats) / np.log(len(price_hats))), normed=True)
     plt.title("Residues")
-    plt.xlabel("$price - \\widehat{price}$")
+    plt.xlabel("$%s - \\widehat{%s}$" % (yvar_name, yvar_name, ))
 
     plt.tight_layout()
     plt.savefig(filename, dpi=300)
 
     system("open ./%s" % (filename, ))
+
+def _calculate_r2_adj(y_vector, x_matrix, beta_vector):
+    y_hats = np.dot(x_matrix, beta_vector)
+
+    y_mean = sum(y_vector) / len(y_vector)
+
+    rss = sum([(y_vector[i] - y_hats[i])**2 for i in xrange(len(y_vector))])
+    tss = sum([(y_vector[i] - y_mean)**2 for i in xrange(len(y_vector))])
+
+    n = len(y_vector)
+    k = x_matrix.shape[1]
+
+    return 1.0 - (rss / (n - k)) / (tss / (n - 1))
 
 def pr05((bal, brick, d2, d3, d4, dist, floor, price, totsp, walk, ), var_number):
     template_vars = {}
@@ -63,7 +76,52 @@ def pr05((bal, brick, d2, d3, d4, dist, floor, price, totsp, walk, ), var_number
 
     template_vars['linear_betas'] = beta_vector
 
-    _save_and_show_plot(price, x_matrix, beta_vector, "~figure01.png")
+    _save_and_show_plot(price, x_matrix, beta_vector, filename="~figure01.png")
+
+    template_vars['linear_plot_url'] = "~figure01.png"
+
+    template_vars['linear_rss'] = residues
+    template_vars['linear_r2'] = _calculate_r2_adj(y_vector, x_matrix, beta_vector)
+
+    # Semilog model
+    y_vector = np.log(y_vector)
+
+    beta_vector, residues, rank, s = np.linalg.lstsq(x_matrix, y_vector)
+
+    template_vars['semilog_betas'] = beta_vector
+
+    _save_and_show_plot(np.log(price), x_matrix, beta_vector, filename="~figure02.png", yvar_name="\ln{price}")
+
+    template_vars['semilog_plot_url'] = "~figure02.png"
+
+    template_vars['semilog_rss'] = residues
+    template_vars['semilog_r2'] = _calculate_r2_adj(y_vector, x_matrix, beta_vector)
+
+    # Log model
+    x_matrix = np.array([
+        np.ones(len(price)),
+        np.log(totsp),
+        np.log(dist),
+        walk,
+        d2,
+        d3,
+        d4,
+        bal,
+        brick,
+        floor,
+        ]).transpose()
+
+    beta_vector, residues, rank, s = np.linalg.lstsq(x_matrix, y_vector)
+
+    template_vars['log_betas'] = beta_vector
+
+    _save_and_show_plot(np.log(price), x_matrix, beta_vector, filename="~figure03.png", yvar_name="\ln{price}")
+
+    template_vars['log_plot_url'] = "~figure03.png"
+
+    template_vars['log_rss'] = residues
+    template_vars['log_r2'] = _calculate_r2_adj(y_vector, x_matrix, beta_vector)
+
 
     print _render_template(template_vars).encode('utf-8')
 
