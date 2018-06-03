75bdf3dc2d3f485b6c2c8770ac7f756c6dbdf7e1
Ilya Gulkov <isgulkov@edu.hse.ru>
2018-06-03 14:20:02 +0300

    Abstract experiments and plotting apart into two separate classes

--- problem2018-3/solution.py
+++ problem2018-3/solution.py
@@ -7,45 +7,19 @@ from matplotlib import pyplot as plt, cm
 from matplotlib.gridspec import GridSpec
 
 
-class Experiment:
-    def __init__(self, Dist, dist_name, criteria, crit_names, sample_sizes=(8, 50), n_repeat=10000, alpha=0.05):
-        self.Dist = Dist
+class ResultsPlot:
+    def __init__(self, dist_name, crit_names):
         self.dist_name = dist_name
-        self.criteria = criteria
         self.crit_names = crit_names
-        self.sample_sizes = sample_sizes
-        self.n_repeat = n_repeat
-        self.alpha = alpha
-
-    def _rejection_rates(self, sample_pairs):
-        nx_reject = np.zeros(len(self.criteria))
-
-        print "Counting rejections on", sample_pairs.shape[0], "samples..."
-
-        for a_sample, b_sample in sample_pairs:
-            for i, crit in enumerate(self.criteria):
-                _, p_value = crit(a_sample, b_sample)
-
-                if p_value < self.alpha:
-                    nx_reject[i] += 1
-
-            if i % 100 == 0:
-                print i
-
-        return nx_reject / sample_pairs.shape[0]
+        self.results = []
 
     def _ith_color(self, i_crit):
-        return cm.Set1(float(i_crit) / len(self.criteria))
+        return cm.Set1(float(i_crit) / len(self.crit_names))
 
-    def _create_sample_pairs(self, sample_size):
-        return self.Dist().rvs(size=(self.n_repeat, 2, sample_size))
+    def _display_false_positives(self, ax, fpos_rates):
+        fpos_rates *= 100.0
 
-    def _display_false_positives(self, ax, sample_size):
-        rej_rx = self._rejection_rates(self._create_sample_pairs(sample_size))
-
-        rej_rx *= 100.0
-
-        for i, rate in enumerate(rej_rx):
+        for i, rate in enumerate(fpos_rates):
             bar_rect = ax.bar([i], [rate], 0.8, align='center', color=self._ith_color(i), label=self.crit_names[i])[0]
 
             ax.text(
@@ -60,8 +34,8 @@ class Experiment:
 
         ax.yaxis.grid()
 
-        ax.set_xlim(-0.5, len(self.criteria) - 0.5)
-        ax.set_ylim(0.0, max(rej_rx) * 1.5)
+        ax.set_xlim(-0.5, len(fpos_rates) - 0.5)
+        ax.set_ylim(0.0, max(fpos_rates) * 1.5)
 
         ax.set_xticklabels(self.crit_names)
         ax.set_xticks(range(len(self.crit_names)))
@@ -86,26 +60,15 @@ class Experiment:
 
         return d_line
 
-    def _display_powers(self, ax, sample_size, cx=np.arange(0.0, 4.01, 0.5)):
-        crits_rates = []
-
-        for c in cx:
-            sample_pairs = self._create_sample_pairs(sample_size)
-
-            sample_pairs[:,1,:] += c
-
-            crits_rates.append(
-                self._rejection_rates(sample_pairs)
-            )
-
-        crits_rates = np.transpose(crits_rates) * 100.0
+    def _display_powers(self, ax, cx, power_plots):
+        power_plots *= 100.0
 
         f_lines = []
         df_lines = []
 
         d_ax = ax.twinx()
 
-        for i, (rates, name) in enumerate(zip(crits_rates, self.crit_names)):
+        for i, (rates, name) in enumerate(zip(power_plots, self.crit_names)):
             f_line = ax.plot(cx, rates, color=self._ith_color(i), marker='.', label=name)
             f_lines.append(f_line[0])
 
@@ -127,6 +90,11 @@ class Experiment:
         all_lines = [line for p in zip(f_lines, df_lines) for line in p]
         d_ax.legend(all_lines, [line.get_label() for line in all_lines], loc='lower right', prop={'size': 'x-small'}, markerscale=1.5)
 
+    def add_result(self, sample_size, fpos_rates, cx, power_plots):
+        self.results.append(
+            (sample_size, fpos_rates, cx, power_plots)
+        )
+
     def display_results(self):
         fig = plt.figure(figsize=(12, 12))
 
@@ -135,22 +103,17 @@ class Experiment:
             self.dist_name
         ))
 
-        gs = GridSpec(len(self.sample_sizes), 2)
+        gs = GridSpec(len(self.results), 2)
 
-        for i, sample_size in enumerate(self.sample_sizes):
-            print "Sample size", sample_size
-
+        for i, (sample_size, fpos_rates, cx, power_plots) in enumerate(self.results):
             ax_false_pos = fig.add_subplot(gs[i, 0])
 
-            self._display_false_positives(ax_false_pos, sample_size)
+            self._display_false_positives(ax_false_pos, fpos_rates)
 
             self._display_powers(
                 fig.add_subplot(gs[i, 1]),
-                sample_size,
-                cx=np.concatenate([
-                    np.linspace(0.0, 1.0, num=10)[:-1],
-                    np.linspace(1.0, 4.0, num=10)
-                ])
+                cx,
+                power_plots
             )
 
             left_loc = ax_false_pos.get_position()
@@ -165,10 +128,89 @@ class Experiment:
         return fig
 
 
+class TwoSampleExperiment:
+    def __init__(self, Dist, criteria, n_repeat=10000, alpha=0.05):
+        self.Dist = Dist
+        self.criteria = criteria
+        self.n_repeat = n_repeat
+        self.alpha = alpha
+
+    def _rejection_rates(self, sample_pairs):
+        nx_reject = np.zeros(len(self.criteria))
+
+        print "Counting rejections on", sample_pairs.shape[0], "samples..."
+
+        for a_sample, b_sample in sample_pairs:
+            for i, crit in enumerate(self.criteria):
+                _, p_value = crit(a_sample, b_sample)
+
+                if p_value < self.alpha:
+                    nx_reject[i] += 1
+
+            if i % 100 == 0:
+                print i
+
+        return nx_reject / sample_pairs.shape[0]
+
+    def _create_sample_pairs(self, sample_size):
+        return self.Dist().rvs(size=(self.n_repeat, 2, sample_size))
+
+    def get_false_positives(self, sample_size):
+        print "Calculating f.pos, n =", sample_size
+
+        return self._rejection_rates(self._create_sample_pairs(sample_size))
+
+    def get_powers(self, sample_size, cx):
+        print "Calculating powers, n =", sample_size
+
+        rate_plots = []
+
+        for c in cx:
+            sample_pairs = self._create_sample_pairs(sample_size)
+
+            sample_pairs[:,1,:] += c
+
+            rate_plots.append(
+                self._rejection_rates(sample_pairs)
+            )
+
+        return np.transpose(rate_plots)
+
+
 from scipy.stats import ttest_ind, ttest_rel, ranksums
 
 
 def display_results(Dist, dist_name):
+    e = TwoSampleExperiment(
+        Dist,
+        (
+            ttest_ind,
+            # ttest_rel,
+            ranksums,
+        ),
+        n_repeat=100
+    )
+
+    plot = ResultsPlot(
+        dist_name,
+        (
+            "Student (ind.)",
+            # "Student (rel.)",
+            "Wilcoxon",
+        )
+    )
+
+    cx = np.concatenate([
+        np.linspace(0.0, 1.0, num=10)[:-1],
+        np.linspace(1.0, 4.0, num=10)
+    ])
+
+    for sample_size in (8, 50):
+        fposx = e.get_false_positives(sample_size)
+        powers = e.get_powers(sample_size, cx)
+
+        plot.add_result(sample_size, fposx, cx, powers)
+
     plt.rcParams['font.family'] = 'serif'
 
     plt.rcParams['xtick.labelsize'] = 'x-small'
@@ -176,22 +218,7 @@ def display_results(Dist, dist_name):
     plt.rcParams['ytick.labelsize'] = 'x-small'
     plt.rcParams['ytick.color'] = '#545454'
 
-    fig = Experiment(
-        lambda: Dist(),
-        dist_name,
-        (
-            ttest_ind,
-            # ttest_rel,
-            ranksums,
-        ),
-        (
-            "Student (ind.)",
-            # "Student (rel.)",
-            "Wilcoxon",
-        ),
-        sample_sizes=(8, 50),
-        n_repeat=1000
-    ).display_results()
+    fig = plot.display_results()
 
     fig.savefig("~figure.png", dpi=300)
     system("open ./~figure.png")
